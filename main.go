5100b5b71ceb10bbe1ffdf89ac520af38bcd6368
mirza akhena <mirza.akhena@gmail.com>
2020-10-31 23:54:55 +0700

    remove command and query method name

--- main.go
+++ main.go
@@ -99,13 +99,13 @@ func main() {
 			os.Exit(0)
 		}
 
-	// case "outports":
+	case "outports":
 
-	// 	gen := gogen.NewOutport()
-	// 	if err := gen.Generate(os.Args...); err != nil {
-	// 		fmt.Printf("%s\n", err.Error())
-	// 		os.Exit(0)
-	// 	}
+		gen := gogen.NewOutport()
+		if err := gen.Generate(os.Args...); err != nil {
+			fmt.Printf("%s\n", err.Error())
+			os.Exit(0)
+		}
 
 	default:
 		fmt.Printf("command %s is not recognized. %s\n", command, message)
