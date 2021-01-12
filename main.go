fa5f23aad9ec1ff433a204cfcc354f8a2372f005
Mirza Akhena <mirza.akhena@gmail.com>
2021-01-12 22:47:57 +0700

    fixing usage

--- main.go
+++ main.go
@@ -10,91 +10,15 @@ import (
 
 func usage() {
 
-	const message string = `
-try execute gogen by those command:
-
-
-
-1.
-gogen usecase <usecase name> [<method1> <method2> <method3> ... ]
-
-This command is for creating new usecase
-sample:
-
+	const message string = `try execute gogen by those command:
 	gogen usecase CreateOrder
-	or
 	gogen usecase CreateOrder Publish Save
-
-CreateOrder is a usecase name with PascalCase
-Publish and Save is an Outport method
-
-	
-	
-2.	
-gogen outport usecase <method1> <method2> <method3> ...
-
-This command is for adding new outport to existing usecase
-sample:
-
-	gogen outports CreateOrder Validate AnotherCheck
-	
-CreateOrder is a usecase name with PascalCase	
-Validate and AnotherCheck is a new method in usecase's outport
-
-
-
-3.
-gogen test <usecase name>
-
-This command is for create test template for specific usecase
-sample:
-		
-	gogen test CreateOrder	
-
-CreateOrder is a usecase name with PascalCase	
-
-
-
-4.
-gogen gateway <gateway name> <usecase name>
-
-This command is for create gateway
-sample:
-	
-	gogen gateway Production CreateOrder
-
-Production is a gateway name
-CreateOrder is a usecase name with PascalCase		
-
-
-
-5.
-gogen controller <controller name> <usecase name>
-
-This command is for create controller
-sample:
-
-	gogen controller Restapi CreateOrder
-
-Restapi is a controller name
-CreateOrder is a usecase name with PascalCase			
-
-
-
-6.
-gogen registry <registry name> <controller name> <usecase name> <gateway name>
-
-This command is for bind controller usecase and gateway
-sample:
-
-	gogen registry Default Restapi CreateOrder Production
-
-Default is a registry name
-Restapi is a controller name
-CreateOrder is a usecase name with PascalCase
-Production is a gateway name
-
-
+	gogen outports CreateOrder Validate Publish Save
+	gogen test CreateOrder
+	gogen gateway production CreateOrder
+	gogen controller restapi CreateOrder
+	gogen registry default restapi CreateOrder production
+	gogen init
 `
 	fmt.Fprintf(os.Stdout, "%s\n", message)
 }
