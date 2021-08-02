a9a405f628e1100aab4ba0243944902cf2de1033
mirza akhena <mirza.akhena@dana.id>
2021-08-02 18:30:03 +0700

    adding check entity name if exist

--- gogencommand/entity.go
+++ gogencommand/entity.go
@@ -3,10 +3,12 @@ package gogencommand
 import (
 	"flag"
 	"fmt"
-	"strings"
-
 	"github.com/mirzaakhena/gogen/templates"
 	"github.com/mirzaakhena/gogen/util"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strings"
 )
 
 type EntityModel struct {
@@ -31,6 +33,50 @@ func (obj *EntityModel) Run() error {
 	err := util.CreateFolderIfNotExist("domain/entity")
 	if err != nil {
 		return err
+	}
+
+	{
+
+		fset := token.NewFileSet()
+
+		pkgs, err := parser.ParseDir(fset, "domain/entity", nil, parser.ParseComments)
+		if err != nil {
+			if err != nil {
+				return err
+			}
+		}
+
+		for _, pkg := range pkgs {
+			for _, file := range pkg.Files {
+
+				for _, decl := range file.Decls {
+
+					gen, ok := decl.(*ast.GenDecl)
+					if !ok || gen.Tok != token.TYPE {
+						continue
+					}
+
+					for _, specs := range gen.Specs {
+
+						ts, ok := specs.(*ast.TypeSpec)
+						if !ok {
+							continue
+						}
+
+						if _, ok = ts.Type.(*ast.StructType); !ok {
+							continue
+						}
+
+						// entity already exist, abort the command
+						if ts.Name.String() == fmt.Sprintf("%s", obj.EntityName) {
+							return nil
+						}
+					}
+				}
+
+			}
+		}
+
 	}
 
 	{
