ffdfd15e08a4f3f85be76339d2f6c78af97ba49a
mirza <mirza.akhena@gmail.com>
2022-12-20 10:16:12 +0700

    update code

--- command/gendomain/command.go
+++ command/gendomain/command.go
@@ -1,13 +1,8 @@
 package gendomain
 
 import (
-	"embed"
-	"encoding/json"
 	"fmt"
-	"github.com/mirzaakhena/gogen/utils/model"
 	"math/rand"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/mirzaakhena/gogen/utils"
@@ -72,34 +67,9 @@ func Run(inputs ...string) error {
 		return err
 	}
 
-	err = CopyPasteFolder(".gogen/templates", "controller")
+	err = utils.CreateGogenConfig(err, domainName)
 	if err != nil {
 		return err
-	}
-
-	err = CopyPasteFolder(".gogen/templates", "gateway")
-	if err != nil {
-		return err
-	}
-
-	// handle gogen/config.json
-	{
-		gogenDomainFile := "./.gogen/gogenrc.json"
-
-		data := model.GogenConfig{
-			Domain:     domainName,
-			Controller: "default",
-			Gateway:    "default",
-		}
-
-		jsonInBytes, err := json.MarshalIndent(data, "", " ")
-		if err != nil {
-			return err
-		}
-		_, err = utils.WriteFileIfNotExist(string(jsonInBytes), gogenDomainFile, struct{}{})
-		if err != nil {
-			return err
-		}
 	}
 
 	// handle .gitignore
@@ -115,107 +85,6 @@ func Run(inputs ...string) error {
 		_, err = utils.WriteFileIfNotExist(string(fileBytes), "./.gitignore", struct{}{})
 		if err != nil {
 			return err
-		}
-
-	}
-
-	return nil
-
-}
-
-func CopyPasteFolder(destFolder, sourceFolder string) error {
-
-	ff := FileAndFolders{
-		Folders: map[string]int{},
-		Files:   make([]string, 0),
-	}
-
-	templateController := fmt.Sprintf("templates/%s", sourceFolder)
-	err := readFolders(utils.AppTemplates, templateController, templateController, &ff)
-	if err != nil {
-		return err
-	}
-
-	for folder := range ff.Folders {
-		err := os.MkdirAll(fmt.Sprintf("%s/%s/%v", destFolder, sourceFolder, folder), 0755)
-		if err != nil {
-			return err
-		}
-	}
-
-	for _, fileRaw := range ff.Files {
-
-		file, err := os.Create(fmt.Sprintf("%s/%s/%s", destFolder, sourceFolder, fileRaw))
-		if err != nil {
-			return err
-		}
-
-		fileInBytes, err := utils.AppTemplates.ReadFile(fmt.Sprintf("templates/%s/%s", sourceFolder, fileRaw))
-		if err != nil {
-			return err
-		}
-
-		_, err = file.WriteString(string(fileInBytes))
-		if err != nil {
-			return err
-		}
-
-		err = file.Sync()
-		if err != nil {
-			return err
-		}
-
-		err = file.Close()
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-type FileAndFolders struct {
-	Folders map[string]int
-	Files   []string
-}
-
-func readFolders(efs embed.FS, skip, path string, ff *FileAndFolders) error {
-
-	dirs, err := efs.ReadDir(path)
-	if err != nil {
-		return err
-	}
-
-	for _, dir := range dirs {
-
-		name := dir.Name()
-
-		if dir.IsDir() {
-
-			s := fmt.Sprintf("%s/%s", path, name)
-			//fmt.Printf("found folder %s\n", s)
-
-			for k := range ff.Folders {
-				//fmt.Printf("k=%v\n", k)
-				if strings.Contains(s, k) {
-					//fmt.Printf("remove %v from %v\n", k, ff.Folders)
-					delete(ff.Folders, k)
-				}
-			}
-
-			ff.Folders[s[len(skip)+1:]] = 1
-
-			err = readFolders(efs, skip, fmt.Sprintf("%s/%s", path, name), ff)
-			if err != nil {
-				return err
-			}
-
-		} else {
-			s := fmt.Sprintf("%s/%s", path, name)
-			//fmt.Printf("found file   %s\n", s)
-			fileName := s[len(skip)+1:]
-			//fmt.Printf("%v\n", fileName)
-			ff.Files = append(ff.Files, fileName)
 		}
 
 	}
---
+++ utils/config.go
@@ -0,0 +1,139 @@
+package utils
+
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+	"github.com/mirzaakhena/gogen/utils/model"
+	"os"
+	"strings"
+)
+
+func CreateGogenConfig(err error, domainName string) error {
+	err = CopyPasteFolder(".gogen/templates", "controller")
+	if err != nil {
+		return err
+	}
+
+	err = CopyPasteFolder(".gogen/templates", "gateway")
+	if err != nil {
+		return err
+	}
+
+	// handle gogen/config.json
+	{
+		gogenDomainFile := "./.gogen/gogenrc.json"
+
+		data := model.GogenConfig{
+			Domain:     domainName,
+			Controller: "default",
+			Gateway:    "default",
+		}
+
+		jsonInBytes, err := json.MarshalIndent(data, "", " ")
+		if err != nil {
+			return err
+		}
+		_, err = WriteFileIfNotExist(string(jsonInBytes), gogenDomainFile, struct{}{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func CopyPasteFolder(destFolder, sourceFolder string) error {
+
+	ff := FileAndFolders{
+		Folders: map[string]int{},
+		Files:   make([]string, 0),
+	}
+
+	templateController := fmt.Sprintf("templates/%s", sourceFolder)
+	err := readFolders(AppTemplates, templateController, templateController, &ff)
+	if err != nil {
+		return err
+	}
+
+	for folder := range ff.Folders {
+		err := os.MkdirAll(fmt.Sprintf("%s/%s/%v", destFolder, sourceFolder, folder), 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, fileRaw := range ff.Files {
+
+		file, err := os.Create(fmt.Sprintf("%s/%s/%s", destFolder, sourceFolder, fileRaw))
+		if err != nil {
+			return err
+		}
+
+		fileInBytes, err := AppTemplates.ReadFile(fmt.Sprintf("templates/%s/%s", sourceFolder, fileRaw))
+		if err != nil {
+			return err
+		}
+
+		_, err = file.WriteString(string(fileInBytes))
+		if err != nil {
+			return err
+		}
+
+		err = file.Sync()
+		if err != nil {
+			return err
+		}
+
+		err = file.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func readFolders(efs embed.FS, skip, path string, ff *FileAndFolders) error {
+
+	dirs, err := efs.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	for _, dir := range dirs {
+
+		name := dir.Name()
+
+		if dir.IsDir() {
+
+			s := fmt.Sprintf("%s/%s", path, name)
+			//fmt.Printf("found folder %s\n", s)
+
+			for k := range ff.Folders {
+				//fmt.Printf("k=%v\n", k)
+				if strings.Contains(s, k) {
+					//fmt.Printf("remove %v from %v\n", k, ff.Folders)
+					delete(ff.Folders, k)
+				}
+			}
+
+			ff.Folders[s[len(skip)+1:]] = 1
+
+			err = readFolders(efs, skip, fmt.Sprintf("%s/%s", path, name), ff)
+			if err != nil {
+				return err
+			}
+
+		} else {
+			s := fmt.Sprintf("%s/%s", path, name)
+			//fmt.Printf("found file   %s\n", s)
+			fileName := s[len(skip)+1:]
+			//fmt.Printf("%v\n", fileName)
+			ff.Files = append(ff.Files, fileName)
+		}
+
+	}
+
+	return nil
+
+}
